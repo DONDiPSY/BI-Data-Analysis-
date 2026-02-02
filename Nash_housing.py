029d568126b71e4c268a2912f8bd72c51ac93932
ADEDOLAPO AYODEJI <ayodejiadedolapo@gmail.com>
2026-02-02 13:07:43 -0700

    Updated Dateset by make bridge between Python and database

--- Nash_housing.py
+++ Nash_housing.py
@@ -1,7 +1,17 @@
+# operating system
+import os
 #import libraries
 import numpy as np
 import pandas as pd
+# Creates a database connection engine in python
+# (The bridge between Python and database)
 from sqlalchemy import create_engine
+# Safely encode Special characters in string (especially Username and Passwords)
+from urllib.parse import  quote_plus
+#Load environment variable from a .env file into Python
+from dotenv import load_dotenv
+# Encrypting and decrypting sensitive data
+from cryptography.fernet import  Fernet
 # to read the csv file
 df = pd.read_csv("NashvilleHousing.csv")
 backup_df = df.copy()
@@ -83,3 +93,37 @@ df.info()
 
 
 print(df.isnull().sum())
+
+# Removing space in columns name and columns data
+def Column_Space_data(df):
+    df.columns = df.columns.str.strip()
+    df = df.map(lambda x :x.strip() if isinstance(x, str) else x)
+    return df
+
+
+Column_Space_data(df)
+
+# checking for duplicates
+print(df.duplicated().any())
+
+# Load enviroment varaible
+load_dotenv()
+
+# Retrieve Key and encrypted password from .env
+key = os.getenv("DB_KEY").encode()
+encrypted_user =os.getenv("DB_USER").encode()
+encrypted_pass =os.getenv("DB_PASS").encode()
+
+# Decrypt username and Password with key
+Cipher = Fernet(key)
+decrypt_user = Cipher.decrypt(encrypted_user).decode()
+decrypt_pass = Cipher.decrypt(encrypted_pass).decode()
+
+# safely encode the username and password  for the url
+username = quote_plus(decrypt_user)
+password = quote_plus(decrypt_pass)
+
+# Exporting  clean dataset to mysql
+# The bridge between Python and database
+engine = create_engine(f"mysql+pymysql://{username}:{password}@localhost/nashvilla_housingdb")
+df.to_sql("nash_housing", con= engine, if_exists='replace', index=True)
